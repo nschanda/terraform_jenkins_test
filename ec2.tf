d98ae33d9167f5ed67cc0dbb43b0237c067d333a
nschanda <96650080+nschanda@users.noreply.github.com>
2023-06-27 09:27:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,10 @@ provider "aws" {
     region  = "ap-south-1"
 }
 
+
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-076754bea03bde973"
+  ami           = "ami-057752b3f1d6c4d6c"
   instance_type = "t2.micro"
 
   tags = {
